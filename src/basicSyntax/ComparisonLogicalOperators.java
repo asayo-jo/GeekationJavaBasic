adf4d42a60d2117afff72f608fdf2d8be76cfdb4
asayo-jo <a.jo.cingroup2012@gmail.com>
2024-06-13 22:58:22 +0900

    比較演算子と論理演算子の実装

--- src/basicSyntax/ComparisonLogicalOperators.java
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -8,27 +8,27 @@ public class ComparisonLogicalOperators {
 		int a = 20;
 		int b = 10;
 		boolean resultA = (a > b);
-		
+
 		System.out.println(resultA);
-		
+
 		//2.
 		boolean isSunny = true;
 		boolean isWarm = true;
 		boolean resultB = (isSunny && isWarm);
 		
 		System.out.println(resultB);
-		
+
 		//3.
 		int x = 10;
 		int y = 2;
 		boolean resultC = (x >= 0 && y % 2 == 0 );
-		
+
 		System.out.println(resultC);
-		
+
 		//4.
 		boolean hasPermission = false;
 		boolean resultD = !hasPermission;
-		
+
 		System.out.println(resultD);
 	}
 }
